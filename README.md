0779338c43df4c8df64f249396ea47454af72f76
Evandro P. Santos <evandro.santos@soulisto.com.br>
2020-04-27 15:55:10 -0300

    Project

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# command-line-tool
-Ferramenta de Linha de Comando
+# Ferramenta de linha de Comando
+
+Sistema de CRUD de herois
+
+**Indice**
+1. [Setup e Build](#1-setup-e-build)
+2. [Opções](#2-opcoes)
+3. [Licença](#3-licenca)
+
+## 1. Setup e Build
+``` bash
+# instalar dependências (execute o comando na raiz do projeto)
+$ npm install
+
+# lista de opções
+$ npm start
+
+# rodar os testes
+$ npm test
+```
+
+## 2. Opções
+
+- -n, --nome -> Insere o Nome do Heroi
+- -p, --poder -> Inser o Poder do Heroi
+- -i, --id -> Insere o Id do Heroi
+
+- -c, --cadastrar -> Cadastra um novo heroi
+- -l, --listar -> Lista os herois
+- -r, --remover -> Remove um heroi pelo id
+- -a, --atualizar -> Atualizar um heroi pelo id
+
+## 3. Licença
+Este projeto está licenciado sob a licença MIT - consulte o [LICENSE.md](LICENSE.md) para mais detalhes.
---
+++ index.js
@@ -0,0 +1,71 @@
+const Commander = require('commander')
+const Database = require('./database')
+const Heroi = require('./heroi')
+
+async function main() {
+  Commander
+    .version('v1')
+    .option('-n, --nome [value]', "Nome do Heroi")
+    .option('-p, --poder [value]', "Poder do Heroi")
+    .option('-i, --id [value]', "Id do Heroi")
+
+    .option('-c, --cadastrar', "Cadastrar um novo heroi")
+    .option('-l, --listar', "Lista os herois")
+    .option('-r, --remover', "Remove um heroi pelo id")
+    .option('-a, --atualizar [value]', "Atualizar um heroi pelo id")
+    .parse(process.argv)
+
+  const heroi = new Heroi(Commander)
+
+  try {
+    if (Commander.cadastrar) {
+      delete heroi.id
+
+      const resultado = await Database.cadastrar(heroi)
+
+      if (!resultado) {
+        console.log('Heroi nao foi cadastrado!')
+        return
+      }
+
+      console.log('Heroi cadastrado com sucesso!')
+    }
+
+    if (Commander.listar) {
+      const resultado = await Database.listar()
+      console.log(resultado)
+      return
+    }
+
+    if (Commander.remover) {
+      const resultado = await Database.remover(heroi.id)
+
+      if (!resultado) {
+        console.error('Nao foi possivel remover o heroi')
+      }
+
+      console.log('Heroi removido com sucesso!')
+    }
+
+    if (Commander.atualizar) {
+      const idParaAtualizar = parseInt(Commander.atualizar)
+
+      // Remover todas as chaves que estiverem undefined | null
+      const dado = JSON.stringify(heroi)
+      const heroiAtualizar = JSON.parse(dado)
+
+      const resultado = await Database.atualizar(idParaAtualizar, heroiAtualizar)
+
+      if (!resultado) {
+        console.error('Nao foi possivel atualizar o heroi')
+        return
+      }
+
+      console.log('Heroi atualizado com sucesso!')
+    }
+  } catch(error) {
+    Console.error('Deu Ruim ', error)
+  }
+}
+
+main()
